289bcfd3fe7ba134ae2ba4929aa15a509d9e22ef
Vucomir Ianculov <vukomir@ianculov.ro>
2025-05-19 13:29:02 +0300

    feat: add CloudWatch Alarm SNS topic and email subject configuration #major

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -106,19 +106,3 @@ resource "aws_cloudwatch_metric_alarm" "example" {
 
   tags = local.tags
 }
-
-# Output the module outputs
-output "lambda_function_name" {
-  description = "The name of the Lambda function"
-  value       = module.cloudwatch_logs_notifier.lambda_function_name
-}
-
-output "sns_topic_arn" {
-  description = "The ARN of the SNS topic for CloudWatch log notifications"
-  value       = module.cloudwatch_logs_notifier.sns_topic_arn
-}
-
-output "cloudwatch_alarm_sns_topic_arn" {
-  description = "The ARN of the SNS topic for CloudWatch alarm notifications"
-  value       = module.cloudwatch_logs_notifier.cloudwatch_alarm_sns_topic_arn
-}
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -7,3 +7,18 @@ output "sns_topic_arn" {
   description = "ARN of the created SNS topic"
   value       = module.cloudwatch_logs_notifier.sns_topic_arn
 }
+
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = module.cloudwatch_logs_notifier.lambda_function_name
+}
+
+output "sns_topic_arn" {
+  description = "The ARN of the SNS topic for CloudWatch log notifications"
+  value       = module.cloudwatch_logs_notifier.sns_topic_arn
+}
+
+output "cloudwatch_alarm_sns_topic_arn" {
+  description = "The ARN of the SNS topic for CloudWatch alarm notifications"
+  value       = module.cloudwatch_logs_notifier.cloudwatch_alarm_sns_topic_arn
+}
